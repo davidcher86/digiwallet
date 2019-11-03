e0d76bc75f478426c5ccdd05860640a666279340
davidcher86 <davidcher86@gmail.com>
2019-11-03 22:42:59 +0200

    - for sync

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,6 @@ class App extends Component {
     };
 
     renderContent() {
-        // console.log(this.state.loggedIn);
         const styles = {
             containerStyle: {},
             spinnerStyle: {
