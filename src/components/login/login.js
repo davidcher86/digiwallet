57c9030b1a40b70c1cadef0bd1a5c7f6d7f7eeab
davidcher86 <davidcher86@gmail.com>
2019-11-04 00:13:31 +0200

    - style to login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, TouchableHighlight, Text } from 'react-native';
+import { View, TouchableOpacity, Text } from 'react-native';
 import { Button, Input, Icon  } from 'react-native-elements';
 import { connect } from 'react-redux';
 import firebase from 'firebase';
@@ -23,16 +23,13 @@ class LoginForm extends Component {
     renderButton(props) {
         const styles = {
             buttonContainerStyle: {
-                padding: 5,
-                height: 15
-                // flexDirection: 'row',
-                // flex: 1
+                backgroundColor: '#2980b6',
+                paddingVertical: 15
             },
             buttonStyle: {
-                // margin: 5
-                // height: 30
-                // flexDirection: 'row',
-                // flex: 1
+                color: '#fff',
+                textAlign: 'center',
+                fontWeight: '700'
             }
         };
 
@@ -41,6 +38,7 @@ class LoginForm extends Component {
             return (
                 <View style={styles.buttonContainerStyle}>
                     <Button
+                        style={styles.buttonStyle}
                         title="Loading button"
                         loading />
                 </View>);
@@ -48,11 +46,15 @@ class LoginForm extends Component {
 
         return (
             <View style={styles.buttonContainerStyle}>
-                <Button
+                <TouchableOpacity style={styles.buttonContainer} 
+                                  onPress={this.onLoginPress.bind(this)}>
+                    <Text  style={styles.buttonStyle}>LOGIN</Text>
+                </TouchableOpacity>
+                {/* <Button
                     type="outline"
                     style={styles.buttonStyle}
                     onPress={this.onLoginPress.bind(this)}
-                    title="Log In" />
+                    title="Log In" /> */}
             </View>);
 
     };
@@ -60,23 +62,34 @@ class LoginForm extends Component {
     render() {
         const styles = {
             containerStyle: {
-                marginTop:  30,
+                padding: 20
             },
             labelStyle: {},
-            inputStyle: {}
+            inputStyle: {
+                height: 40,
+                backgroundColor: 'rgba(225,225,225,0.2)',
+                marginBottom: 10,
+                padding: 10,
+                color: '#fff'
+            }
         };
 
         return(
             <View style={styles.containerStyle}>
                 <Input
                     placeholder='user@gmail.com'
+                    style={styles.inputStyle}
+                    autoCorrect={false}
                     value={this.props.login.username}
                     onChangeText={text => this.props.changeUsername(text)}
                     leftIcon={{ name: 'mail' }}
+                    autoCapitalize="none"
                     errorStyle={{ color: 'red' }}
+                    placeholderTextColor='rgba(225,225,225,0.7)'
                     label='Email' />
                 <Input
                     placeholder='123456'
+                    style={styles.inputStyle}
                     value={this.props.login.password}
                     onChangeText={text => this.props.changePassword(text)}
                     leftIcon={{ name: 'mail' }}
